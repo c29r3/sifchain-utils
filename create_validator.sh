365404a03ae25b1623d0b2244825ab2b16cd9bd8
Sergey <52304443+c29r3@users.noreply.github.com>
2020-11-04 16:56:48 +0300

    Update create_validator.sh

--- create_validator.sh
+++ create_validator.sh
@@ -1,15 +1,19 @@
-sifnodecli tx staking create-validator \
-  --amount=9000000trwn \
-  --pubkey=$(sifnoded tendermint show-validator) \
-  --moniker=$(cat /root/sifchain/acc_name.txt) \
-  --chain-id=monkey-bars \
-  --commission-rate="0.10" \
-  --commission-max-rate="0.20" \
-  --commission-max-change-rate="0.01" \
-  --min-self-delegation="1" \
-  --gas="auto" \
-  --gas-adjustment="1.2" \
-  --gas-prices="0.025trwn" \
-  --from=sifchain \
-  --yes \
-  --node tcp://localhost:27657
+while true;
+do
+  sifnodecli tx staking create-validator \
+    --amount=9000000trwn \
+    --pubkey=$(sifnoded tendermint show-validator) \
+    --moniker=$(cat /root/sifchain/acc_name.txt) \
+    --chain-id=monkey-bars \
+    --commission-rate="0.10" \
+    --commission-max-rate="0.20" \
+    --commission-max-change-rate="0.01" \
+    --min-self-delegation="1" \
+    --gas="auto" \
+    --gas-adjustment="1.2" \
+    --gas-prices="0.025trwn" \
+    --from=sifchain \
+    --yes \
+    --node tcp://localhost:27657
+  sleep 15
+done
