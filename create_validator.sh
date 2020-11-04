2456a7ca41568a359a6fd51050855a0ad4411c03
Sergey <52304443+c29r3@users.noreply.github.com>
2020-11-04 17:04:28 +0300

    Update create_validator.sh

--- create_validator.sh
+++ create_validator.sh
@@ -1,8 +1,8 @@
 while true;
 do
-  /root/bin/sifnodecli tx staking create-validator \
+  /root/go/bin/sifnodecli tx staking create-validator \
     --amount=9000000trwn \
-    --pubkey=$(/root/bin/sifnoded tendermint show-validator) \
+    --pubkey=$(/root/go/bin/sifnoded tendermint show-validator) \
     --moniker=$(cat /root/sifchain/acc_name.txt) \
     --chain-id=monkey-bars \
     --commission-rate="0.10" \
