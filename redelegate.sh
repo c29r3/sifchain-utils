9fe6969d7efb62f5802f97d8c663346179181b64
Sergey <52304443+c29r3@users.noreply.github.com>
2020-11-06 18:40:42 +0300

    Create redelegate.sh

---
+++ redelegate.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+BIN_FILE="/root/go/bin/sifnodecli"
+WALLET_NAME="sifchain"
+CHAIN_ID="monkey-bars"
+SELF_ADDR=$($BIN_FILE keys list -o json | jq -r .[0].address)
+DENOM="trwn"
+OPERATOR=$($BIN_FILE q staking delegations --chain-id $CHAIN_ID -o json --node tcp://localhost:27657 $SELF_ADDR | jq -r .[].validator_address)
+
+echo -e "Current address: $SELF_ADDR\nCurrent operator address: $OPERATOR"
+
+while true;
+do
+    BALANCE=$($BIN_FILE query account $SELF_ADDR -o json --node tcp://localhost:27657 | jq -r .value.coins[0].amount)
+    echo CURRENT BALANCE IS: $BALANCE
+    REWARD=$(( $BALANCE - 200000 ))
+
+    if (( $BALANCE >  899999 )); then
+        echo "Let's delegate $REWARD of REWARD tokens to $SELF_ADDR"
+        # delegate balance
+        $BIN_FILE tx staking delegate $OPERATOR "$REWARD"$DENOM --chain-id $CHAIN_ID --node tcp://localhost:27657 --gas-adjustment 1.5 --gas="200000" --fees 7500$DENOM --from $WALLET_NAME -y
+
+    else
+        echo "Reward is $REWARD"
+    fi
+    sleep 500
+done
+echo "DONE"
