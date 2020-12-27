22d4466126a628fd847a53d6781bcdb6339b9bdc
Sergey <52304443+c29r3@users.noreply.github.com>
2020-12-27 06:53:45 +0300

    Update create_validator.sh

--- create_validator.sh
+++ create_validator.sh
@@ -1,6 +1,7 @@
+PSWD="12345678"
 while true;
 do
-  /root/go/bin/sifnodecli tx staking create-validator \
+  echo -e "$PSWD\n$PSWD\n" | /root/go/bin/sifnodecli tx staking create-validator \
     --amount=1000000rowan \
     --pubkey=$(/root/go/bin/sifnoded tendermint show-validator) \
     --moniker=$(cat /root/sifchain/acc_name.txt) \
