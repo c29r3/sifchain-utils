35de758b02ba0001505096d65424b2c76b133613
Sergey <52304443+c29r3@users.noreply.github.com>
2020-12-27 06:50:43 +0300

    Update create_validator.sh

--- create_validator.sh
+++ create_validator.sh
@@ -4,7 +4,7 @@ do
     --amount=1000000rowan \
     --pubkey=$(/root/go/bin/sifnoded tendermint show-validator) \
     --moniker=$(cat /root/sifchain/acc_name.txt) \
-    --chain-id=monkey-bars \
+    --chain-id=merry-go-round \
     --commission-rate="0.10" \
     --commission-max-rate="0.20" \
     --commission-max-change-rate="0.01" \
