5eb4438d4dac2c82b3a5abf2057f78d3c8c0012f
Sergey <52304443+c29r3@users.noreply.github.com>
2021-02-20 17:59:02 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
+# MAINNET
 PROJECT_NAME="sifchain"
-MONIKER=$(cat $HOME/$PROJECT_NAME/acc_name.txt)
-CHAIN_ID="monkey-bars"
+MONIKER="c29r3 | StakeTab"
+CHAIN_ID="sifchain"
 BIN_PATH="$HOME/go/bin"
 CLI="sifnodecli"
 NODED="sifnoded"
-PERSISTENT_PEERS="ec03640d0dcb1160f8cf73c33c63b64a55c93906@35.166.247.98:26656,04fad3abcf8d5c6d94d7815f9485830c280a8d73@35.166.247.98:28002,330c1b876d916f7518562b33d2749e3d1fcf7817@35.166.247.98:28004,16d9c23623e42723dfcf3dcbb11d98d989689a7a@35.166.247.98:28006,44746bbabd4707f36728164ec59cb04890d7f019@161.97.107.5:26656"
+PERSISTENT_PEERS="8c240f71f9e060277ce18dc09d82d3bbb05d1972@13.211.43.177:26656,0120f0a48e7e81cc98829ef4f5b39480f11ecd5a@52.76.185.17:26656,bcc2d07a14a8a0b3aa202e9ac106dec0bef91fda@13.55.247.60:26656"
 
 echo "-> INSTALL REQUIREMENTS"
 apt update \
@@ -15,18 +16,16 @@ systemctl stop $PROJECT_NAME
 rm -rf $HOME/.$NODED $HOME/.$CLI
   
 echo "---> COMPILE BINARY FILES"
-mkdir ~/$PROJECT_NAME; \
-cd ~/$PROJECT_NAME; \
-git clone https://github.com/Sifchain/sifnode.git; \
-cd sifnode; \
-make install; \
-$BIN_PATH/$NODED version --long
+wget https://github.com/Sifchain/sifnode/releases/download/mainnet-relayer-20210221063300/sifnoded-mainnet-relayer-20210221063300-linux-amd64.zip
+unzip sifnoded*zip
+mv sifnode* ~/go/bin
+rm sifnode*zip
 
 echo "----> INIT CONFIG FILE"
 $BIN_PATH/$NODED init $MONIKER --chain-id $CHAIN_ID
 
 echo "-----> DOWNLOAD GENESIS FILE"
-curl -s https://raw.githubusercontent.com/Sifchain/networks/feature/genesis/testnet/monkey-bars-testnet-4/genesis.json | jq . > $HOME/.$NODED/config/genesis.json
+curl http://44.235.108.41:26657/genesis | jq '.result.genesis' > ~/.sifnoded/config/genesis.json
 
 
 sed -i 's|prof_laddr = "localhost:6060"|prof_laddr = "localhost:6091"|g' $HOME/.$NODED/config/config.toml
@@ -40,13 +39,11 @@ sed -i 's|laddr = "tcp://127.0.0.1:26657"|laddr = "tcp://127.0.0.1:27657"|g' $HO
 sed -i "s|persistent_peers = \"\"|persistent_peers = \"$PERSISTENT_PEERS\"|g" $HOME/.$NODED/config/config.toml
 
 $BIN_PATH/$CLI config trust-node true
-$BIN_PATH/$CLI config keyring-backend test
 $BIN_PATH/$CLI config chain-id $CHAIN_ID
 
-echo "Generating new key"
-echo "yes\n" | $BIN_PATH/$CLI keys add $PROJECT_NAME --keyring-backend test -o json &> $HOME/$PROJECT_NAME/$PROJECT_NAME\_$MONIKER\_key.json
-
-cat $HOME/$PROJECT_NAME/$PROJECT_NAME_key.json | jq -r .
+#echo "Generating new key"
+#echo "yes\n" | $BIN_PATH/$CLI keys add $PROJECT_NAME --keyring-backend test -o json &> $HOME/$PROJECT_NAME/$PROJECT_NAME\_$MONIKER\_key.json
+#cat $HOME/$PROJECT_NAME/$PROJECT_NAME_key.json | jq -r .
 
 echo "------> Creating systemd unit $PROJECT_NAME.service"
 tee /etc/systemd/system/$PROJECT_NAME.service > /dev/null <<EOF  
